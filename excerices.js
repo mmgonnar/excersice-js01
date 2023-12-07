12820357bd3cf85e738dfac3d9328bf7a0009fc1
mmgonnar <gn.mariela@gmail.com>
2023-12-07 15:59:42 -0600

    excercise

---
+++ excerices.js
@@ -0,0 +1,6 @@
+const samurai = "7 Samuráis";
+const knights = "12 Caballeros de la Mesa Redonda";
+const spartans = "300 espartanos";
+const totalHeroes = parseInt(samurai, 10) + parseInt(knights, 10) + parseInt(spartans, 10);
+
+      console.log(totalHeroes);
